19b7deec21189e243495dc840b2bb4969ba0dc76
Pete Hodgson <git@thepete.net>
2022-03-16 17:02:37 -0700

    basic rate limiter

--- src/lib/tokenBucketLimiter.test.ts
+++ src/lib/tokenBucketLimiter.test.ts
@@ -6,6 +6,7 @@ describe(TokenBucketLimiter, () => {
     const succeeded = limiter.requestConsumeTokens(1)
     expect(succeeded).toBe(true)
   });
+
   test('bucket eventually runs out of room', () => {
     const limiter = new TokenBucketLimiter(10,10)
     limiter.requestConsumeTokens(5)
@@ -13,4 +14,22 @@ describe(TokenBucketLimiter, () => {
     const succeeded = limiter.requestConsumeTokens(5)
     expect(succeeded).toBe(false)
   });
+
+  test('bucket refills after running out of room', async () => {
+    const limiter = new TokenBucketLimiter(10,5)
+
+    limiter.requestConsumeTokens(9)
+    while(limiter.requestConsumeTokens(1)){}
+
+    await sleep(2000)
+    const succeeded = limiter.requestConsumeTokens(5)
+
+    expect(succeeded).toBe(true)
+  });
 });
+
+function sleep(duration:number){
+  return new Promise((resolve) => {
+      setTimeout(resolve, duration);
+  });
+}
--- src/lib/tokenBucketLimiter.ts
+++ src/lib/tokenBucketLimiter.ts
@@ -8,17 +8,28 @@ export class TokenBucketLimiter {
   constructor(capacity,fillRate){
     this.bucketCapacity = capacity
     this.tokenRate = fillRate
-    this._tokenCount = 0
+    this._tokenCount = capacity
     this._timeOfLastFill = new Date()
   }
 
   // request to perform an operation that would consume a number of tokens
   requestConsumeTokens(tokenCount:number):boolean{
-    if(this.bucketCapacity < tokenCount){
+    this.refresh()
+
+    if(this._tokenCount < tokenCount){
       return false;
     }
 
-    this.bucketCapacity -= tokenCount
+    this._tokenCount -= tokenCount
     return true
+  }
+
+  private refresh(){
+    const now = new Date();
+    const delta = now.getTime() - this._timeOfLastFill.getTime()
+
+    const newTokens = this.tokenRate/1000*delta
+    this._tokenCount += newTokens
+    this._timeOfLastFill = new Date()
   }
 }
